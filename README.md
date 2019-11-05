24a6bbf3ccbc7823d8b523d2f7499f39599c5025
Lucas Lopes Cardoso <lucas.cardoso@solutis.net.br>
2019-11-05 11:33:35 -0300

    update README

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # workeezy
-Workeezy
+Projeto iniciado na disciplina de Economia da Inovação.
+
+## O que é o Workeezy?
+
+É uma plataforma que visa gamificar atividades corporativas de novos colaboradores, com foco especial/inicial em estagiários, aprendizes e trainees. Buscando aumentar o engajamento e melhorar a qualidade no aprendizado e adaptação dos novos colaboradores à empresa.
+
+## Instruções de instalação do projeto:
+
+Antes de mais nada, verifique que possui uma versão do PHP e uma do NodeJS instalada em seu computador, caso já possua, digite o comando no terminal:
+
+`npm install` ou, se preferir (eu recomendo) `yarn` (sim, apenas yarn *-*)
+
+Agora rode o comando para instalar as dependências do _Composer_
+
+`composer install`
+
+Após isso, será necessário copiar o conteúdo do arquivo de configuração _".env.example"_ para um novo arquivo _".env"_, utilize o comando:
+
+###### Caso use Windows
+`copy .env.example .env`
+
+###### Caso use algum SO baseado em Unix (Linux, MacOS...)
+`cp .env.example .env`
+
+Agora digite o comando pra gerar uma key do Laravel para sua aplicação
+
+`php artisan key:generate`
+
+### Beleza, mas como eu visualizo o projeto no navegador?
+
+Simples, basta rodar primeiro o servidor do artisan com o comando:
+
+`php artisan serve`
+
+E logo depois rodar o servidor do webpack para monitorar as alterações no código:
+
+`npm run watch` ou `yarn watch` (recomento usar o yarn s2)
+
+#### Autenticação e serviços:
+
+Verifique que possui uma base de dados (_mysql_) com o nome _workeezy_ criada, caso já possua, rode o comando para criar as tabelas utilizando as _migrations_ e os _seeders_ (atualmente existe as migrations padrão do laravel 6.x):
+
+`php artisan migrate --seed`
+
+### Pronto, agora é só meter a mão na massa e #BORACODAR!!!
--- public/js/app.js
+++ public/js/app.js
@@ -1494,7 +1494,7 @@ module.exports = function spread(callback) {
 
 
 var bind = __webpack_require__(/*! ./helpers/bind */ "./node_modules/axios/lib/helpers/bind.js");
-var isBuffer = __webpack_require__(/*! is-buffer */ "./node_modules/is-buffer/index.js");
+var isBuffer = __webpack_require__(/*! is-buffer */ "./node_modules/axios/node_modules/is-buffer/index.js");
 
 /*global toString:true*/
 
@@ -1825,6 +1825,28 @@ module.exports = {
   extend: extend,
   trim: trim
 };
+
+
+/***/ }),
+
+/***/ "./node_modules/axios/node_modules/is-buffer/index.js":
+/*!************************************************************!*\
+  !*** ./node_modules/axios/node_modules/is-buffer/index.js ***!
+  \************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+/*!
+ * Determine if an object is a Buffer
+ *
+ * @author   Feross Aboukhadijeh <https://feross.org>
+ * @license  MIT
+ */
+
+module.exports = function isBuffer (obj) {
+  return obj != null && obj.constructor != null &&
+    typeof obj.constructor.isBuffer === 'function' && obj.constructor.isBuffer(obj)
+}
 
 
 /***/ }),
@@ -6270,28 +6292,6 @@ module.exports = {
 
 }));
 //# sourceMappingURL=bootstrap.js.map
-
-
-/***/ }),
-
-/***/ "./node_modules/is-buffer/index.js":
-/*!*****************************************!*\
-  !*** ./node_modules/is-buffer/index.js ***!
-  \*****************************************/
-/*! no static exports found */
-/***/ (function(module, exports) {
-
-/*!
- * Determine if an object is a Buffer
- *
- * @author   Feross Aboukhadijeh <https://feross.org>
- * @license  MIT
- */
-
-module.exports = function isBuffer (obj) {
-  return obj != null && obj.constructor != null &&
-    typeof obj.constructor.isBuffer === 'function' && obj.constructor.isBuffer(obj)
-}
 
 
 /***/ }),
@@ -68913,8 +68913,8 @@ if (document.getElementById("example")) {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\Workeezy\Workeezy\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\Workeezy\Workeezy\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\lucas.cardoso\Documents\workeezy\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\lucas.cardoso\Documents\workeezy\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
